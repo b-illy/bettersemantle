509a457791c6b7c041ed69c574f07e13779a72b3
billy <19840447+b-illy@users.noreply.github.com>
2022-02-14 23:51:24 +0000

    clearer commenting

--- semantle.user.js
+++ semantle.user.js
@@ -2,7 +2,7 @@
 // @name           Better Semantle
 // @description    allows you to play unlimited random words instead of just one a day
 // @author         billy
-// @version        2.0
+// @version        2.0.1
 // @match          https://semantle.novalis.org/*
 // @downloadURL    https://b-illy.github.io/bettersemantle/semantle.user.js
 // @updateURL      https://b-illy.github.io/bettersemantle/semantle.user.js
@@ -12,17 +12,18 @@
 let payload = async () => {
   setTimeout(async () => {
     // local storage handling
-    let storage = window.localStorage;
+    const storage = window.localStorage; // easier to type, lazy shortcut
     
     if (!(storage.getItem("random") == "0" || storage.getItem("random") == "1")) {
       storage.setItem("random", "0"); // set to daily (false/off/0) by default
     }
     
+    // set this bool according to local storage
     let random = false;
     if (storage.getItem("random") == "1") random = true;
     
 
-    // copied from semantle src - gets scores of 1st, 10th, 1000th nearest words
+    // basically copied from semantle src - gets scores of 1st, 10th, 1000th nearest words
     let getSimilarityStory = async (secret) => {
       const url = "/similarity/" + secret;
       const response = await fetch(url);
@@ -33,16 +34,16 @@ let payload = async () => {
       }
     }
     
-    let changeWord = (r) => {
+    const changeWord = (r) => {
       // clear guesses
       storage.removeItem("guesses");
       storage.removeItem("winState");
       storage.removeItem("currentWord");
 
-      // toggle on "random" variable
+      // set "random" toggle to whatever parameter was given
       storage.setItem("random", r);
 
-      // reload page
+      // reload page (required to update page after clearing guesses)
       window.location.reload();
     }
     
@@ -50,14 +51,14 @@ let payload = async () => {
     
     let dailyBtn = document.createElement("button");
     dailyBtn.innerHTML = "Go back to daily word";
-    dailyBtn.onclick = (e) => { changeWord("0"); }
+    dailyBtn.onclick = (e) => { changeWord("0"); } // set to daily word
     
     let changeWordBtn = document.createElement("button");
     changeWordBtn.innerHTML = "Play a new random word";
-    changeWordBtn.onclick = (e) => { changeWord("1"); }
+    changeWordBtn.onclick = (e) => { changeWord("1"); } // set to new random word
     
     
-    // update daily word info text
+    // update daily word info ("similarity story") text and add buttons
     if (random) {
       // change secret word to a random one (if none stored)
       if (storage.getItem("currentWord")) {
@@ -67,24 +68,26 @@ let payload = async () => {
         storage.setItem("currentWord", secret);
       }
       
-      // update similarity story
+      // update similarity story for current word (rather than the daily)
       similarityStory = await getSimilarityStory(secret);
       simStory.innerHTML = `The nearest word has a similarity of <b>${(similarityStory.top * 100).toFixed(2)}</b>, the tenth-nearest has a similarity of ${(similarityStory.top10 * 100).toFixed(2)} and the one thousandth nearest word has a similarity of ${(similarityStory.rest * 100).toFixed(2)}.`;
       simStory.innerHTML += "<br>(This secret word was randomised.)";
       
-      // add daily/random toggle
-      
+      // add "back to daily" button
       simStory.parentNode.insertBefore(dailyBtn, simStory.nextSibling);
+      // add "new random word" button (BEFORE "back to daily")
       simStory.parentNode.insertBefore(changeWordBtn, simStory.nextSibling);
-      changeWordBtn.after(" ");
+      changeWordBtn.after(" "); // (add whitespace between the buttons)
     } else {
+      // add "new random word" button only
       simStory.parentNode.insertBefore(changeWordBtn, simStory.nextSibling);
     }
-  }, 1000);
+  }, 1000); // execute after 1s to make sure main semantle js has executed first, a bit hacky
 }
 
 
-let inject = (func) => {
+// adds a function to the page in a new script tag
+const inject = (func) => {
   let source = func.toString();
   let script = document.createElement("script");
   script.innerHTML = "(" + source + ")()";
